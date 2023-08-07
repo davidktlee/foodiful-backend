c56d04266a8813c68c8cc8bcf28f5f634373133d
davidktlee <rudxor567@naver.com>
2023-08-07 21:56:19 +0900

    feat: 토큰 방식 프론트에게 넘기는 방식 설정 및 prisma migrate

--- nest-app/src/auth/guards/jwt-refresh.guard.ts
+++ nest-app/src/auth/guards/jwt-refresh.guard.ts
@@ -1,11 +1,3 @@
-import {
-  CanActivate,
-  ExecutionContext,
-  UnauthorizedException,
-} from '@nestjs/common';
-import { JwtService } from '@nestjs/jwt';
 import { AuthGuard } from '@nestjs/passport';
-import { Request } from 'express';
-import { AuthService } from '../auth.service';
 
 export class JwtRefreshGuard extends AuthGuard('jwt-refresh-token') {}
